fab8e1a18fe51162b6494d46d1894f23cf4ea5df
codepharmacist <40608063+codepharmacist@users.noreply.github.com>
2018-07-01 10:39:45 +0100

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,47 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+
+function makeGrid() {
+	// Your code goes here!
+	// Select size input
+	
+	var canvas, cell, gridHeight, gridWidth, rows;
+	
+	canvas = $('#pixel_canvas');
+	gridHeight = $('#input_height').val();
+	gridWidth = $('#input_width').val();
+	
+	canvas.children().remove()
+	
+	for (x = 0; x < gridHeight; x++) {
+	canvas.append('<tr></tr>');
+	}
+	
+	rows = $('tr');
+	
+	for (y = 0; y < gridWidth; y++) {
+	rows.append('<td></td>');
+	} 
+	
+	cell = canvas.find('td');
+	cell.click(function() {
+		var color;
+		color = $("#colorPicker").val();
+		$(this).attr('bgcolor', color);
+	});
+	
+}
+
+var submitQuery;
+
+submitQuery = $('input[type="submit"]')
+
+submitQuery.click(function(event) {
+
+  event.preventDefault();
+  makeGrid();
+});
+
